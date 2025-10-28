7c0824507181e1a6d08adff1a803edd330aff00a
Edgar <edr9719@outlook.com>
2025-10-27 22:56:19 -0600

    Initial version without Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,132 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import ProductList from './components/ProductList';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch('/api/products');
+        if (!res.ok) throw new Error('Failed To Fetch Products');
+        const data = await res.json();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='min-h-screen bg-gray-100 p-6'>
+      <div className='flex'>
+        <svg
+          height='38px'
+          width='38px'
+          viewBox='0 0 1024 1024'
+          className='icon'
+          version='1.1'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='#000000'
+        >
+          <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+          <g
+            id='SVGRepo_tracerCarrier'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+          ></g>
+          <g id='SVGRepo_iconCarrier'>
+            <path
+              d='M845.4 383H758c-16.6 0-30-13.4-30-30s13.4-30 30-30h87.4c16.6 0 30 13.4 30 30s-13.5 30-30 30zM662.3 383H263.1c-16.6 0-30-13.4-30-30s13.4-30 30-30h399.2c16.6 0 30 13.4 30 30s-13.5 30-30 30z'
+              fill='#97beb1'
+            ></path>
+            <path
+              d='M433.2 873.2m-55 0a55 55 0 1 0 110 0 55 55 0 1 0-110 0Z'
+              fill='#FFB89A'
+            ></path>
+            <path
+              d='M854.5 873.2m-55 0a55 55 0 1 0 110 0 55 55 0 1 0-110 0Z'
+              fill='#FFB89A'
+            ></path>
+            <path
+              d='M889.8 722.1h-511c-37.7 0-68.4-30.7-68.4-68.4v-1.4L274.5 270v-0.2-0.2l-6-55.4c-8.6-86.8-57.6-117.5-97.3-128.1L101.5 69c-16.1-4-32.3 5.9-36.3 22s5.9 32.3 22 36.3l68.9 16.9c16.2 4.3 28.1 12.4 36.6 24.7 8.6 12.4 14 29.7 16.1 51.4l6 55.6 35.6 379.3c0.8 70.1 58.1 126.9 128.4 126.9h511c16.6 0 30-13.4 30-30s-13.4-30-30-30z'
+              fill='#45484C'
+            ></path>
+            <path
+              d='M840.3 197.8H381c-16.6 0-30 13.4-30 30s13.4 30 30 30h459.3c30.2 0 54.9 24.3 55.5 54.3l-19.9 226.5-0.1 1.3v1.3c0 30.6-24.9 55.5-55.5 55.5H436c-16.6 0-30 13.4-30 30s13.4 30 30 30h384.3c63.2 0 114.7-51.1 115.5-114.1L955.7 316l0.1-1.3v-1.3c0-63.8-51.8-115.6-115.5-115.6z'
+              fill='#45484C'
+            ></path>
+            <path
+              d='M408.5 842.1c7.2 0 13.1 5.9 13.1 13.1s-5.9 13.1-13.1 13.1-13.1-5.9-13.1-13.1 5.9-13.1 13.1-13.1m0-60c-40.4 0-73.1 32.7-73.1 73.1s32.7 73.1 73.1 73.1 73.1-32.7 73.1-73.1-32.7-73.1-73.1-73.1zM823.1 842.1c7.2 0 13.1 5.9 13.1 13.1s-5.9 13.1-13.1 13.1-13.1-5.9-13.1-13.1 5.9-13.1 13.1-13.1m0-60c-40.4 0-73.1 32.7-73.1 73.1s32.7 73.1 73.1 73.1 73.1-32.7 73.1-73.1-32.7-73.1-73.1-73.1z'
+              fill='#45484C'
+            ></path>
+          </g>
+        </svg>
+        <h1 className='text-3xl font-bold mb-6'>Product Category</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {loading && <p>Loading...</p>}
+      {error && (
+        <div className='error flex text-xl gap-2 items-center'>
+          <svg
+            height='28px'
+            width='28px'
+            viewBox='0 0 25 25'
+            version='1.1'
+            xmlns='http://www.w3.org/2000/svg'
+            xmlns:xlink='http://www.w3.org/1999/xlink'
+            xmlns:sketch='http://www.bohemiancoding.com/sketch/ns'
+            fill='#d10000'
+            stroke='#d10000'
+          >
+            <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+            <g
+              id='SVGRepo_tracerCarrier'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            ></g>
+            <g id='SVGRepo_iconCarrier'>
+              {' '}
+              <title>cross</title> <desc>Created with Sketch Beta.</desc>{' '}
+              <defs> </defs>{' '}
+              <g
+                id='Page-1'
+                stroke='none'
+                stroke-width='1'
+                fill='none'
+                fill-rule='evenodd'
+                sketch:type='MSPage'
+              >
+                {' '}
+                <g
+                  id='Icon-Set'
+                  sketch:type='MSLayerGroup'
+                  transform='translate(-467.000000, -1039.000000)'
+                  fill='#ff0000'
+                >
+                  {' '}
+                  <path
+                    d='M489.396,1061.4 C488.614,1062.18 487.347,1062.18 486.564,1061.4 L479.484,1054.32 L472.404,1061.4 C471.622,1062.18 470.354,1062.18 469.572,1061.4 C468.79,1060.61 468.79,1059.35 469.572,1058.56 L476.652,1051.48 L469.572,1044.4 C468.79,1043.62 468.79,1042.35 469.572,1041.57 C470.354,1040.79 471.622,1040.79 472.404,1041.57 L479.484,1048.65 L486.564,1041.57 C487.347,1040.79 488.614,1040.79 489.396,1041.57 C490.179,1042.35 490.179,1043.62 489.396,1044.4 L482.316,1051.48 L489.396,1058.56 C490.179,1059.35 490.179,1060.61 489.396,1061.4 L489.396,1061.4 Z M485.148,1051.48 L490.813,1045.82 C492.376,1044.26 492.376,1041.72 490.813,1040.16 C489.248,1038.59 486.712,1038.59 485.148,1040.16 L479.484,1045.82 L473.82,1040.16 C472.257,1038.59 469.721,1038.59 468.156,1040.16 C466.593,1041.72 466.593,1044.26 468.156,1045.82 L473.82,1051.48 L468.156,1057.15 C466.593,1058.71 466.593,1061.25 468.156,1062.81 C469.721,1064.38 472.257,1064.38 473.82,1062.81 L479.484,1057.15 L485.148,1062.81 C486.712,1064.38 489.248,1064.38 490.813,1062.81 C492.376,1061.25 492.376,1058.71 490.813,1057.15 L485.148,1051.48 L485.148,1051.48 Z'
+                    id='cross'
+                    sketch:type='MSShapeGroup'
+                  >
+                    {' '}
+                  </path>{' '}
+                </g>{' '}
+              </g>{' '}
+            </g>
+          </svg>
+          {error}
+        </div>
+      )}
 
-export default App
+      <ProductList products={products} />
+    </div>
+  );
+};
+
+export default App;
